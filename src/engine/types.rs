6833c4d320db98a802cd61ec7fedcf0e10a71473
Udit Karode <udit.karode@gmail.com>
2023-09-24 04:17:45 +0530

    doc: add note

--- src/engine/types.rs
+++ src/engine/types.rs
@@ -14,5 +14,7 @@ pub struct XYPair {
 pub struct ObjectInfo {
     // in case we want to support window resizing in the
     // future, the objects might want to know the size.
+    // this is however a bad example, because it doesn't
+    // need to be set per game object, so let's fix that later
     pub window_size: WindowSize,
 }
